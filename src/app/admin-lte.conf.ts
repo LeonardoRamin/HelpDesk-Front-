961620171d5352f9aa3488d47d4209e346eb951d
Leonardo Ramin Santos Lima <leonardoramin@outlook.com>
2020-01-31 16:14:44 -0300

    Starting to edit the template

--- src/app/admin-lte.conf.ts
+++ src/app/admin-lte.conf.ts
@@ -10,27 +10,16 @@ export const adminLteConf = {
   // layout: 'normal',
   sidebarLeftMenu: [
     {label: 'MAIN NAVIGATION', separator: true},
-    {label: 'Get Started', route: '/', iconClasses: 'fa fa-road', pullRights: [{text: 'New', classes: 'label pull-right bg-green'}]},
-    {label: 'Layout', iconClasses: 'fa fa-th-list', children: [
-        {label: 'Configuration', route: 'layout/configuration'},
-        {label: 'Custom', route: 'layout/custom'},
-        {label: 'Header', route: 'layout/header'},
-        {label: 'Sidebar Left', route: 'layout/sidebar-left'},
-        {label: 'Sidebar Right', route: 'layout/sidebar-right'},
-        {label: 'Content', route: 'layout/content'}
-      ]},
-    {label: 'COMPONENTS', separator: true},
-    {label: 'Accordion', route: 'accordion', iconClasses: 'fa fa-tasks'},
-    {label: 'Alert', route: 'alert', iconClasses: 'fa fa-exclamation-triangle'},
-    {label: 'Boxs', iconClasses: 'fa fa-files-o', children: [
-        {label: 'Default Box', route: 'boxs/box'},
-        {label: 'Info Box', route: 'boxs/info-box'},
-        {label: 'Small Box', route: 'boxs/small-box'}
-      ]},
-    {label: 'Dropdown', route: 'dropdown', iconClasses: 'fa fa-arrows-v'},
-    {label: 'Form', iconClasses: 'fa fa-files-o', children: [
-        {label: 'Input Text', route: 'form/input-text'}
+    {label: 'Users', iconClasses: 'fa fa-th-list', children: [
+        {label: 'List', route: 'layout/configuration'},
+        {label: 'New User', route: 'layout/custom'},
     ]},
-    {label: 'Tabs', route: 'tabs', iconClasses: 'fa fa-th'}
+    {label: 'Ticket', iconClasses: 'fa fa-th-list', children: [
+      {label: 'List', route: 'layout/configuration'},
+      {label: 'New Ticket', route: 'layout/custom'},
+    ]},
+    {label: 'Users', iconClasses: 'fa fa-th-list', children: [
+      {label: 'To view', route: 'layout/configuration'}
+    ]}
   ]
 };
